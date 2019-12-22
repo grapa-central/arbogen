e578d56cfca15d4936d9a3634b0ce7753005015c
Firat <mersin.firat@yahoo.fr>
2019-12-22 14:19:10 +0100

    counting

--- src/lib/counting.ml
+++ src/lib/counting.ml
@@ -2,39 +2,65 @@
 
 (* type specs : {names: string array; rules: int expression array} *)
 
-let rec count arrays (specs: Grammar.t) (expr: int Grammar.expression) (n: int) (iName: int) (y: int) = (* counting arrays[iName][y] *)
-   let tmp = (Array.get (Array.get arrays iName) y) in
-   if tmp != -1 then tmp
-   else
+let rec countUP arrays (specs: Grammar.t) (expr: int Grammar.expression) (iName: int) (y: int) = 
+     begin
+        match expr with
+	| Z _ -> count arrays specs expr iName y
+	| Union(op1, op2) -> let op1_y = (count arrays specs op1 iName y) in
+			        let op2_y = (count arrays specs op2 iName y) in
+				let res = op1_y + op2_y in Printf.printf "%d + %d = %d\n" op1_y op2_y (op1_y + op2_y); res
+	| Product(op1,op2) -> let sum = ref 0 in
+	    		        for k = 0 to y do
+				   let op1_k = count arrays specs op1 iName k in
+				   let op2_n_k = count arrays specs op2 iName (y-k) in
+				   Printf.printf "n = %d P : %d + %d * %d = %d\n" y !sum op1_k op2_n_k (!sum + op1_k * op2_n_k);
+				   sum := (!sum + op1_k * op2_n_k)
+			        done;
+			        !sum
+				
+	| Reference r -> let tmp = (Array.get (Array.get arrays r) y) in
+  			 if tmp != -1 then tmp 
+			 else let res = count arrays specs (Array.get specs.rules r) r y in res
+	| _ -> print_string "countUnion not handled\n"; -1
+
+     end
+
+
+and count arrays (specs: Grammar.t) (expr: int Grammar.expression) (iName: int) (y: int) = (* counting arrays[iName][y] *)
      begin
         match (expr) with
-        | Z i ->(*print_string "case Z ATOME\n"; *)
-		let res = (if(i == y) then 1 else 0) in
-		     Array.set (Array.get arrays iName) y res;
-		     res
-        | Product(op1, op2) -> (*print_string "case PRODUCT\n";*)
-			       let sum = ref 0 in
-	    		       for k = 0 to y do
-			          let op1_k = count arrays specs op1 n iName k in
-				  let op2_n_k = count arrays specs op2 n iName (y-k) in
-				  sum := (!sum + op1_k * op2_n_k)
-			       done;
-			       Array.set (Array.get arrays iName) y (!sum);
-			       !sum
+        | Z i -> let tmp = (Array.get (Array.get arrays iName) y) in
+		 if tmp != -1 then tmp 
+		 else
+		 let res = (if(i == y) then 1 else 0) in
+	            Printf.printf "Z : arrays[%d][taille:%d] = %d\n" iName y res;
+		    if( Array.get (Array.get arrays iName) y < res) then 
+	               (Array.set (Array.get arrays iName) y res; res)
+	            else res
 
-        |Union(op1, op2) -> (*print_string "case UNION\n";*)
-			    let op1_n = count arrays specs op1 n iName y in
-			    let op2_n = count arrays specs op2 n iName y in
-			    let res = op1_n + op2_n in
-			    Array.set (Array.get arrays iName) y res;
-			    res
+        | Product(_, _) -> let res = countUP arrays specs expr iName y in
+			       Printf.printf "Product : arrays[%d][taille:%d] = %d\n" iName y res;
+			       if( Array.get (Array.get arrays iName) y < res) then 
+			          (Array.set (Array.get arrays iName) y res; res)
+			       else res
 
-	| Reference r -> (*print_string "case REFERENCE ";*)
-		count arrays specs (Array.get specs.rules r) n r y
+        |Union(_, _) -> let res = countUP arrays specs expr iName y in
+			    Printf.printf "Union : arrays[%d][taille:%d] = %d\n" iName y res;
+			    if( Array.get (Array.get arrays iName) y < res) then 
+			       (Array.set (Array.get arrays iName) y res; res)
+			    else res
+
+	| Reference r -> let tmp = (Array.get (Array.get arrays r) y) in
+  		         if tmp != -1 then tmp 
+			 else 
+			 let res = count arrays specs (Array.get specs.rules r) r y in 
+			    Printf.printf "Reference : arrays[%d][taille:%d] = %d\n" iName y res;
+			    if( Array.get (Array.get arrays iName) y < res) then 
+			       (Array.set (Array.get arrays iName) y res; res)
+			    else res 
 
 	| Seq(_) -> -1 (* not handled yet *)
      end
-
 
 let rec hasAtMostAtomeSizeZero (expr: int Grammar.expression) = 
 	match expr with
@@ -80,20 +106,23 @@ let countAll (specs: Grammar.t) n =
    let specSize = (Array.length specs.names) in
    let (countArrays: int array array) = (Array.make_matrix specSize (n+1) (-1)) in
    (* countArrays[i][y] = count(specs.names[i], y), if = -1 not yet computed *)
-   for j = 0 to (specSize-1) do 
+   
+   for j = 0 to (specSize-1) do (* printing names *)  
       Printf.printf "names[%d] = %s\n" j (Array.get specs.names j);
    done;
-   for j = 0 to (specSize-1) do 
+
+   for j = 0 to (specSize-1) do (* counting number of objets of size 0 for each spec in specs *) 
       countSizeZero countArrays specs (Array.get specs.rules j) j
    done;
-   for iName = 0 to (specSize-1) do
-      if ((Array.get (Array.get countArrays iName) 0) == -1) then 
-	(Array.set (Array.get countArrays iName) 0 0) else ()
+
+   for j = 0 to (specSize-1) do (* printing number of objets of size 0 for each spec in specs *) 
+      Printf.printf "count[%d][0] = %d\n" j (Array.get (Array.get countArrays j) 0)
    done;
-   (for iName = 0 to (specSize-1) do
+
+   (for iName = 0 to (specSize-1) do (* counting *)
       (for y = 1 to n do
 	let (expr: int Grammar.expression) = (Array.get specs.rules iName) in
-      	let _ = (count countArrays specs (expr) n iName y) in ()
+      	let _ = (count countArrays specs (expr) iName y) in ()
       done);
    done);
    countArrays
